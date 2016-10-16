4b5bad1095f3de9e559c7ad4053a4e0e1d47a031
Premdeep Saini <premdeepsaini@gmail.com>
2016-10-17 00:17:17 +0530

    Working Date Picker

--- src/main/java/com/principal/validators/DateVatidator.java
+++ src/main/java/com/principal/validators/DateVatidator.java
@@ -8,7 +8,7 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-@FacesValidator("com.principal.validators.DateValidator")
+@FacesValidator("DateValidator")
 public class DateVatidator implements Validator {
 
 	@Override
@@ -21,6 +21,77 @@ public class DateVatidator implements Validator {
 
 		if (dateValue.contains("-")) {
 			String[] dateParts = dateValue.split("-", 3);
+			String dd = dateParts[0];
+			String mm = dateParts[1];
+			String yyyy = dateParts[2];
+
+			int ddi = Integer.parseInt(dd);
+			int mmi = Integer.parseInt(mm);
+			int yyyyi = Integer.parseInt(yyyy);
+
+			if (yyyy.length() == 4 && yyyyi <= currYear) {
+				if (mmi < 1 || mmi > 12) {
+					FacesMessage msg = new FacesMessage("Invalid Date: Month should be between 1 and 12 only");
+					msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+					throw new ValidatorException(msg);
+
+				} else {
+					if (yyyyi % 4 == 0) {
+						if (mmi == 2) {
+							if (ddi < 1 || ddi > 29) {
+								FacesMessage msg = new FacesMessage(
+										"Invalid Date: Date should be between 1 and 29 for February of the year "
+												+ yyyyi);
+								msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+								throw new ValidatorException(msg);
+							}
+						} else if (mmi == 1 || mmi == 3 || mmi == 5 || mmi == 7 || mmi == 8 || mmi == 10 || mmi == 12) {
+							if (ddi < 1 || ddi > 31) {
+								FacesMessage msg = new FacesMessage(
+										"Invalid Date: Date should be between 1 and 31 only");
+								msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+								throw new ValidatorException(msg);
+							}
+						} else {
+							if (ddi < 1 || ddi > 30) {
+								FacesMessage msg = new FacesMessage(
+										"Invalid Date: Date should be between 1 and 30 only");
+								msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+								throw new ValidatorException(msg);
+							}
+						}
+					} else {
+						if (mmi == 2) {
+							if (ddi < 1 || ddi > 28) {
+								FacesMessage msg = new FacesMessage(
+										"Invalid Date: Date should be between 1 and 28 for February");
+								msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+								throw new ValidatorException(msg);
+							}
+						} else if (mmi == 1 || mmi == 3 || mmi == 5 || mmi == 7 || mmi == 8 || mmi == 10 || mmi == 12) {
+							if (ddi < 1 || ddi > 31) {
+								FacesMessage msg = new FacesMessage(
+										"Invalid Date: Date should be between 1 and 31 only");
+								msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+								throw new ValidatorException(msg);
+							}
+						} else {
+							if (ddi < 1 || ddi > 30) {
+								FacesMessage msg = new FacesMessage(
+										"Invalid Date: Date should be between 1 and 30 only");
+								msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+								throw new ValidatorException(msg);
+							}
+						}
+					}
+				}
+			} else {
+				FacesMessage msg = new FacesMessage("Invalid Date:Correct Please enter date in dd-mm-yyyy format");
+				msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+				throw new ValidatorException(msg);
+			}
+		} else if (dateValue.contains("/")) {
+			String[] dateParts = dateValue.split("/", 3);
 			String dd = dateParts[0];
 			String mm = dateParts[1];
 			String yyyy = dateParts[2];
